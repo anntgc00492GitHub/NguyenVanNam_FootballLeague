f27470017f4b580f46c6c44954c11b5e8403241f
anntgc00492GitHub <anntgc00492@gmail.com>
2016-11-15 14:32:33 +0700

    Add project files.

---
+++ NguyenVanNam_FootballLeague.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenVanNam_FootballLeague.Web.MvcApplication" Language="C#" %>
---
+++ NguyenVanNam_FootballLeague.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NguyenVanNam_FootballLeague.Web.Startup))]
+namespace NguyenVanNam_FootballLeague.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
